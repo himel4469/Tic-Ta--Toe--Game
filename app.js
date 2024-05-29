30198558a7c1e08f120a145ec909ad5e12781f77
Himel <Himmelmia>
2024-05-29 22:09:47 +0600

    demo comment

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 'use strict';
+
 let boxes = document.querySelectorAll('.box');
-let ResetBtn = document.querySelector('#Reset_btn');
+let resetBtn = document.querySelector('#Reset_btn');
 let newGame = document.querySelector('#new_btn');
 let msgContainer = document.querySelector('.msg_container');
 let container = document.querySelector('.container');
 let msg = document.querySelector('#msg');
-let tour0 = true;
+let hide = document.querySelector('.hide1');
+
+let isXTurn = true;
+
 const winPatterns = [
   [0, 1, 2],
   [0, 3, 6],
@@ -18,54 +22,74 @@ const winPatterns = [
 ];
 
 const resetGame = () => {
-  let tour0 = true;
+  isXTurn = true;
   enableBoxes();
   msgContainer.classList.add('hide');
 };
 
 const disableBoxes = () => {
-  for (let box of boxes) {
+  boxes.forEach(box => {
     box.disabled = true;
-  }
+  });
 };
+
 const enableBoxes = () => {
-  for (let box of boxes) {
+  boxes.forEach(box => {
     box.disabled = false;
     box.innerText = '';
-  }
-};
-boxes.forEach(box => {
-  box.addEventListener('click', () => {
-    if (tour0) {
-      box.innerText = 'X';
-      tour0 = false;
-    } else {
-      box.innerText = 'O';
-      tour0 = true;
-    }
-    box.disabled = true;
-    checkWinner();
   });
-});
+};
 
-const shoWinner = winner => {
-  msg.innerText = `Congratulation Winner is ${winner}😎`;
+const showWinner = winner => {
+  msg.innerText = `Congratulations! Winner is
+  ${winner}🏆`;
   msgContainer.classList.remove('hide');
   disableBoxes();
 };
 
 const checkWinner = () => {
   for (let pattern of winPatterns) {
-    let pos1val = boxes[pattern[0]].innerText;
-    let pos2val = boxes[pattern[1]].innerText;
-    let pos3val = boxes[pattern[2]].innerText;
-    if (pos1val != '' && pos2val != '' && pos3val != '') {
-      if (pos1val === pos2val && pos2val === pos3val) {
-        shoWinner(pos1val);
-      }
+    const [a, b, c] = pattern;
+    const pos1val = boxes[a].innerText;
+    const pos2val = boxes[b].innerText;
+    const pos3val = boxes[c].innerText;
+
+    if (pos1val && pos1val === pos2val && pos2val === pos3val) {
+      showWinner(pos1val);
+      return;
     }
   }
+  if ([...boxes].every(box => box.innerText !== '')) {
+    msg.innerText = 'The Game is Tie! 🤝';
+    msgContainer.classList.remove('hide');
+    return;
+  }
 };
+// demo comment 
+boxes.forEach(box => {
+  box.addEventListener('click', () => {
+    if (isXTurn) {
+      box.innerText = 'X';
+      box.style.color = '#ff4757';
+      isXTurn = false;
+    } else {
+      box.innerText = 'O';
+      box.style.color = '#2ed573';
+      isXTurn = true;
+    }
+    box.disabled = true;
+    checkWinner();
+  });
+});
 
 newGame.addEventListener('click', resetGame);
-ResetBtn.addEventListener('click', resetGame);
+resetBtn.addEventListener('click', resetGame);
+
+resetGame();
+
+
+
+
+
+
+
